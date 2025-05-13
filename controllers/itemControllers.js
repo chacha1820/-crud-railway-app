c955b2c326381a3189655beede288ff6e5554875
Charity Mae Rosal <charitymaerosal@Charitys-MacBook-Air.local>
2025-05-13 04:45:25 -0700

    Initial commit

--- controllers/itemControllers.js
+++ controllers/itemControllers.js
@@ -1,56 +1,24 @@
-const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
+const db = require('../database/database');
 
-// Connect to SQLite database
-const db = new sqlite3.Database(path.join(__dirname, '../database.db'), (err) => {
-    if (err) return console.error('Failed to connect to DB:', err.message);
-    console.log('Connected to the SQLite database.');
-});
-
-// Create table if not exists
-db.run(`
-    CREATE TABLE IF NOT EXISTS items (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        description TEXT NOT NULL
-    )
-`, (err) => {
-    if (err) console.error('Error creating table:', err.message);
-});
-
-// Get all items
 const getAllItems = (callback) => {
-    db.all('SELECT * FROM items', [], (err, rows) => {
-        callback(err, rows);
-    });
+    db.all('SELECT * FROM items', [], callback);
 };
 
-// Get one item
 const getItemById = (id, callback) => {
-    db.get('SELECT * FROM items WHERE id = ?', [id], (err, row) => {
-        callback(err, row);
-    });
+    db.get('SELECT * FROM items WHERE id = ?', [id], callback);
 };
 
-// Add new item
 const addItem = (name, description, callback) => {
-    db.run('INSERT INTO items (name, description) VALUES (?, ?)', [name, description], function (err) {
-        callback(err, this.lastID);
-    });
+    db.run('INSERT INTO items (name, description) VALUES (?, ?)', [name, description], callback);
 };
 
-// Update item
 const updateItem = (id, name, description, callback) => {
-    db.run('UPDATE items SET name = ?, description = ? WHERE id = ?', [name, description, id], function (err) {
-        callback(err);
-    });
+    db.run('UPDATE items SET name = ?, description = ? WHERE id = ?', [name, description, id], callback);
 };
 
-// Delete item
 const deleteItem = (id, callback) => {
-    db.run('DELETE FROM items WHERE id = ?', [id], function (err) {
-        callback(err);
-    });
+    db.run('DELETE FROM items WHERE id = ?', [id], callback);
 };
 
 module.exports = {
--- index.js
+++ index.js
@@ -68,7 +68,19 @@ app.post('/delete/:id', (req, res) => {
         }
     });
 });
-
+app.get('/edit/:id', (req, res) => {
+    const { id } = req.params;
+    db.get('SELECT * FROM items WHERE id = ?', [id], (err, row) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error retrieving item');
+        } else if (row) {
+            res.render('edit', { item: row }); 
+        } else {
+            res.status(404).send('Item not found');
+        }
+    });
+});
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
